6b8600ed4a4ec734bbfc7e4535d8f8b2a128bd6b
kelvin_arcanjo0 <kelvinarcanjonzinga@gmail.com>
2026-09-16 20:34:48 +0100

    feat(style.css)
    Estilização do Total;

--- script.js
+++ script.js
@@ -257,7 +257,7 @@ function renderCartItems() {
       <img src="${item.image}" alt="${item.name}" class="cart-item-img">
       <div class="cart-item-info">
         <h4>${item.name}</h4>
-        <p>${item.price.toLocaleString('pt-PT')} KZ</p>
+        <p class="cart-item-price">${item.price.toLocaleString('pt-PT')} KZ</p>
       </div>
       <div class="cart-item-controls">
         <button class="btn-qty" data-id="${item.id}" data-action="decrease">-</button>
